9ec066c59b3e488daee6f37ce1b4854eb69aa79a
TK <tgrl@protonmail.com>
2023-01-17 16:30:21 +0300

    Fixed dialogue scripts' namespace

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RPG.Inventories
+namespace RPG.Dialogue
 {
     [CreateAssetMenu(fileName = "Default Dialogue", menuName = "Dialogue/New Dialogue", order = 0)]
     public class Dialogue : ScriptableObject
