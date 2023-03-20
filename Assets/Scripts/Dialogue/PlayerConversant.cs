e703b4faf6e36a003a0805332829da1db194703c
TK <tgrl@protonmail.com>
2023-03-20 19:29:35 +0300

    Implemented displaying the first dialogue node on UI

---
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] private Dialogue currentDialogue;
+
+        public string GetText()
+        {
+            if (currentDialogue == null)
+            {
+                return "Current Dialogue is Null!";
+            }
+            return currentDialogue.DialogueNodes[0].Text;
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Dialogue;
+using TMPro;
+
+namespace RPG.IU
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        private PlayerConversant playerConversant;
+        [SerializeField] private TextMeshProUGUI dialogueText;
+
+        private void Start()
+        {
+            playerConversant = GameObject.FindWithTag("Player").GetComponent<PlayerConversant>();
+            dialogueText.text = playerConversant.GetText();
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
