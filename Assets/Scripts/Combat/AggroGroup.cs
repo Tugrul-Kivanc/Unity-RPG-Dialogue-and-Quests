ed26c2d21460811e9b87b127f84225147bceb7b1
TK <tgrl@protonmail.com>
2023-03-25 17:15:58 +0300

    Implemented aggroing all enemies after dialogue combat outcome

---
+++ Assets/Scripts/Combat/AggroGroup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class AggroGroup : MonoBehaviour
+    {
+        [SerializeField] private Fighter[] fighters;
+        [SerializeField] private bool activateOnStart = false;
+
+        private void Start()
+        {
+            Activate(activateOnStart);
+        }
+        public void Activate(bool shouldActivate)
+        {
+            foreach (Fighter fighter in fighters)
+            {
+                fighter.enabled = shouldActivate;
+
+                CombatTarget target = fighter.GetComponent<CombatTarget>();
+                if (target != null) target.enabled = shouldActivate;
+            }
+        }
+    }
+}
